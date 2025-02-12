c4260c3f417900247f3f158d33e97103c6e2e0ec
阿岛 <m202370863@hust.edu.cn>
2025-02-12 19:44:35 +0800

    FDCAN 命名优化

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -88,22 +88,21 @@ long GetMicros(void) {
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
 
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 	//nSWBOOT0 Setting
 	/*HAL_FLASH_Unlock();
 	 HAL_FLASH_OB_Unlock();
@@ -120,31 +119,31 @@ int main(void)
 	 HAL_FLASH_OB_Lock();
 	 HAL_FLASH_Lock();*/
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_ADC1_Init();
-  MX_ADC2_Init();
-  MX_FDCAN1_Init();
-  MX_SPI1_Init();
-  MX_SPI3_Init();
-  MX_TIM1_Init();
-  MX_USART3_UART_Init();
-  MX_USB_Device_Init();
-  MX_TIM17_Init();
-  MX_TIM16_Init();
-  MX_I2C2_Init();
-  MX_TIM3_Init();
-  /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_DMA_Init();
+	MX_ADC1_Init();
+	MX_ADC2_Init();
+	MX_FDCAN1_Init();
+	MX_SPI1_Init();
+	MX_SPI3_Init();
+	MX_TIM1_Init();
+	MX_USART3_UART_Init();
+	MX_USB_Device_Init();
+	MX_TIM17_Init();
+	MX_TIM16_Init();
+	MX_I2C2_Init();
+	MX_TIM3_Init();
+	/* USER CODE BEGIN 2 */
 	Can_Config(); //Can配置信息
 	HAL_FDCAN_ActivateNotification(&hfdcan1, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0);
 	vofa_start();
@@ -157,6 +156,7 @@ int main(void)
 	UI_Init();
 
 	int TC_INIT_Flag = 0;
+	int Main_Program_Flag = 0;
 	struct angle_speed_torque angle_speed_torque_1 = { 0, 0, 0 };
 	struct angle_speed_torque angle_speed_torque_2 = { 0, 0, 0 };
 	struct angle_speed_torque angle_speed_torque_3 = { 0, 0, 0 };
@@ -168,10 +168,10 @@ int main(void)
 	HAL_TIM_Base_Start_IT(&htim17);
 	HAL_TIM_Base_Start_IT(&htim16);
 
-  /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1) {
 		/*//系统占用率计算
 		 SystemCircleTimes++;
@@ -185,7 +185,7 @@ int main(void)
 		 long SystemTimerLast = GetMicros();*/
 
 		ADC_Read();
-		TensionSensor[0] = (float) ADC_Value2[0] / 4096 * 3.3;//读取传感器信息
+		TensionSensor[0] = (float) ADC_Value2[0] / 4096 * 3.3;	//读取传感器信息
 		TensionSensor[1] = (float) ADC_Value2[1] / 4096 * 3.3;
 		TensionSensor[2] = (float) ADC_Value2[2] / 4096 * 3.3;
 		Home.status[0].num2 = (float) ADC_Value1[0] / 4096 * 26.4;
@@ -209,7 +209,7 @@ int main(void)
 					HAL_Delay(200);
 					TC_INIT_Flag = 1;
 					for (int i = 0; i < 3; i++) {
-						Mset_Pattern[i] = 20;//初始化为力矩模式 设置力矩为零
+						Mset_Pattern[i] = 20;					//初始化为力矩模式 设置力矩为零
 						Mset_Data[i] = 0;
 					}
 				}
@@ -234,7 +234,7 @@ int main(void)
 							estop(0);
 							Home.flag.Label = "ERROR";
 							Home.flag.Color = RED;
-							Home.mode.Label = "OverTorque";//力矩软限制
+							Home.mode.Label = "OverTorque";					//力矩软限制
 							Home.mode.Color = WHITE;
 							Status_Reflash_Flag = 1;
 							Program_Flag[1] = 0;
@@ -261,7 +261,7 @@ int main(void)
 						estop(0);
 						Home.flag.Label = "ERROR";
 						Home.flag.Color = RED;
-						Home.mode.Label = "SignalLost";//信号格式限制
+						Home.mode.Label = "SignalLost";					//信号格式限制
 						Home.mode.Color = WHITE;
 						Status_Reflash_Flag = 1;
 						Program_Flag[1] = 0;
@@ -284,70 +284,64 @@ int main(void)
 				HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, RESET);
 				if (TC_INIT_Flag) {
 					estop(0);
-					HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, RESET);
 				}
 			}
-			if (Program_Flag[1]) {
-				HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, SET);
-			} else {
-				HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, RESET);
-			}
+
 			Motor_Monitor_FLAG = 0;
+
+			HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, Main_Program_Flag);	//主循环工作标识
+			Main_Program_Flag = !Main_Program_Flag;
 		}
 
 		//SystemTimer += (GetMicros() - SystemTimerLast);
 
-    /* USER CODE END WHILE */
+		/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+		/* USER CODE BEGIN 3 */
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
+	/** Configure the main internal regulator output voltage
+	 */
+	HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
-  RCC_OscInitStruct.PLL.PLLN = 42;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV8;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48 | RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
+	RCC_OscInitStruct.PLL.PLLN = 42;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV8;
+	RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
@@ -355,17 +349,16 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1) {
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
