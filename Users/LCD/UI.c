8b827b65f3e4f0ac20a9e89117a9e53904368689
阿岛 <m202370863@hust.edu.cn>
2025-02-27 11:10:45 +0800

    旋转至0度以适配关节驱动器方向

--- Users/LCD/UI.c
+++ Users/LCD/UI.c
@@ -261,7 +261,7 @@ void UI_Init(void) {
 	DEV_Delay_ms(100);
 	LCD_1IN14_Init(HORIZONTAL); //Data
 	DEV_Delay_ms(100);
-	Paint_NewImage(LCD_1IN14.WIDTH, LCD_1IN14.HEIGHT, ROTATE_180, WHITE);
+	Paint_NewImage(LCD_1IN14.WIDTH, LCD_1IN14.HEIGHT, ROTATE_0, WHITE);
 	Paint_SetClearFuntion(LCD_1IN14_Clear);
 	Paint_SetDisplayFuntion(LCD_1IN14_DrawPaint);
 	UI_Startup();
