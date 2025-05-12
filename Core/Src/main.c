11dc97eb7c161dcfb6d74db6753f42ab0dd5ad5d
阿岛 <m202370863@hust.edu.cn>
2025-05-12 16:09:06 +0800

    优化主程序逻辑

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -62,6 +62,7 @@
 /* USER CODE BEGIN PV */
 extern volatile uint8_t offboard_command[3];
 extern volatile float offboard_data[3];
+static uint8_t last_offboard_command[3] = { 0 };
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -85,22 +86,21 @@ long GetMicros(void) {
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
 
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 	//nSWBOOT0 Setting
 	/*HAL_FLASH_Unlock();
 	 HAL_FLASH_OB_Unlock();
@@ -117,33 +117,33 @@ int main(void)
 	 HAL_FLASH_OB_Lock();
 	 HAL_FLASH_Lock();*/
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_ADC1_Init();
-  MX_ADC2_Init();
-  MX_FDCAN1_Init();
-  MX_SPI1_Init();
-  MX_SPI3_Init();
-  MX_TIM1_Init();
-  MX_USART3_UART_Init();
-  MX_USB_Device_Init();
-  MX_TIM17_Init();
-  MX_TIM16_Init();
-  MX_I2C2_Init();
-  MX_TIM3_Init();
-  MX_TIM6_Init();
-  MX_TIM7_Init();
-  /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_DMA_Init();
+	MX_ADC1_Init();
+	MX_ADC2_Init();
+	MX_FDCAN1_Init();
+	MX_SPI1_Init();
+	MX_SPI3_Init();
+	MX_TIM1_Init();
+	MX_USART3_UART_Init();
+	MX_USB_Device_Init();
+	MX_TIM17_Init();
+	MX_TIM16_Init();
+	MX_I2C2_Init();
+	MX_TIM3_Init();
+	MX_TIM6_Init();
+	MX_TIM7_Init();
+	/* USER CODE BEGIN 2 */
 	Can_Config(); //Can配置信息
 	//HAL_FDCAN_ActivateNotification(&hfdcan1, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0);
 	vofa_start();
@@ -176,31 +176,31 @@ int main(void)
 	HAL_TIM_Base_Start_IT(&htim7);
 	HAL_TIM_Base_Start_IT(&htim6);
 
-  /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1) {
 		//系统刷新率计算
 		system_cycle_counter++;
-		/*//系统占用率计算
-		 SystemCircleTimes++;
-		 if ((float) (HAL_GetTick() - SystemClock) > 1000) {
-		 SystemOccupancy = 1 - ((float) (SystemTimer / 1000) / (float) (HAL_GetTick() - SystemClock));
-		 SystemClock = HAL_GetTick();
-		 SystemCircleTimesRecord = SystemCircleTimes;
-		 SystemCircleTimes = 0;
+		//系统占用率计算
+//		 SystemCircleTimes++;
+//		 if ((float) (HAL_GetTick() - SystemClock) > 1000) {
+//		 SystemOccupancy = 1 - ((float) (SystemTimer / 1000) / (float) (HAL_GetTick() - SystemClock));
+//		 SystemClock = HAL_GetTick();
+//		 SystemCircleTimesRecord = SystemCircleTimes;
+//		 SystemCircleTimes = 0;
+//
+//		 }
+//		 long SystemTimerLast = GetMicros();
 
-		 }
-		 long SystemTimerLast = GetMicros();*/
-
-		//Home.status[0].num2 = (float) ADC_Value1[0] / 4096 * 26.4;
-		//Home.status[1].num2 = (float) ADC_Value1[1] / 4096 * 5;
-		//Home.status[2].num2 = SystemOccupancy * 100;
-		Home.status[0].num1 = tension_sensor[0];
-		Home.status[1].num1 = tension_sensor[1];
-		Home.status[2].num1 = tension_sensor[2];
-		parameters_reflash_flag = 1;
+//		Home.status[0].num2 = (float) ADC_Value1[0] / 4096 * 26.4;
+//		Home.status[1].num2 = (float) ADC_Value1[1] / 4096 * 5;
+//		Home.status[2].num2 = SystemOccupancy * 100;
+//		Home.status[0].num1 = tension_sensor[0];
+//		Home.status[1].num1 = tension_sensor[1];
+//		Home.status[2].num1 = tension_sensor[2];
+//		parameters_reflash_flag = 1;
 
 		KEY_Scan();
 
@@ -317,20 +317,29 @@ int main(void)
 					switch (offboard_command[i]) {
 					case 20:
 						Home.params[i].num1 = Torque_Data[i];
-						Paint_DrawString_EN(125, (63 + i * 18), "T->", &Font16, BLACK, GBLUE);
-						HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, SET);
 						break;
 					case 16:
 						Home.params[i].num1 = Angle_Data[i];
-						Paint_DrawString_EN(125, (63 + i * 18), "P->", &Font16, BLACK, GBLUE);
-						HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, SET);
 						break;
 					case 22:
 						Home.params[i].num1 = Speed_Data[i];
-						Paint_DrawString_EN(125, (63 + i * 18), "V->", &Font16, BLACK, GBLUE);
-						HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, SET);
 						break;
 					default:
+					}
+					if (offboard_command[i] != last_offboard_command[i]) {
+						switch (offboard_command[i]) {
+						case 20:
+							Paint_DrawString_EN(125, (63 + i * 18), "T->", &Font16, BLACK, GBLUE);
+							break;
+						case 16:
+							Paint_DrawString_EN(125, (63 + i * 18), "P->", &Font16, BLACK, GBLUE);
+							break;
+						case 22:
+							Paint_DrawString_EN(125, (63 + i * 18), "V->", &Font16, BLACK, GBLUE);
+							break;
+						default:
+						}
+						last_offboard_command[i] = offboard_command[i];
 					}
 				}
 				Drivers.driver1.angle = angle_speed_torque_1.angle;
@@ -357,7 +366,6 @@ int main(void)
 //			 } else {
 //			 HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, RESET);
 
-
 			driver_monitoring_flag = 0;
 
 			HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, main_loop_flag);	//主循环工作标识
@@ -367,57 +375,53 @@ int main(void)
 
 		//SystemTimer += (GetMicros() - SystemTimerLast);
 
-    /* USER CODE END WHILE */
+		/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+		/* USER CODE BEGIN 3 */
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
+	/** Configure the main internal regulator output voltage
+	 */
+	HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
-  RCC_OscInitStruct.PLL.PLLN = 28;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV8;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48 | RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
+	RCC_OscInitStruct.PLL.PLLN = 28;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV8;
+	RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
@@ -425,17 +429,16 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1) {
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
--- Users/TimerCallbacks.c
+++ Users/TimerCallbacks.c
@@ -26,103 +26,104 @@ extern PID_Controller pid_tension_1, pid_tension_2, pid_tension_3;
 int waring_flag = 0;
 HOME original_Home;
 
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-	if (htim == &htim17) { //回传基准时钟5ms
-		HAL_TIM_Base_Start_IT(&htim17);
+void HandleDriverMonitoring() {
+	vofa_send_data(0, Drivers.driver1.angle);
+	vofa_send_data(1, Drivers.driver1.speed);
+	vofa_send_data(2, Drivers.driver1.torque);
+	vofa_send_data(3, Drivers.driver2.angle);
+	vofa_send_data(4, Drivers.driver2.speed);
+	vofa_send_data(5, Drivers.driver2.torque);
+	vofa_send_data(6, Drivers.driver3.angle);
+	vofa_send_data(7, Drivers.driver3.speed);
+	vofa_send_data(8, Drivers.driver3.torque);
+	vofa_send_data(9, tension_sensor[0]);
+	vofa_send_data(10, tension_sensor[1]);
+	vofa_send_data(11, tension_sensor[2]);
+	vofa_send_data(12, (float) system_frequency);
+	vofa_send_data(13, (float) program_mode_code);
+	vofa_sendframetail();
+	driver_monitoring_flag = 1;
+}
 
-		vofa_send_data(0, Drivers.driver1.angle);
-		vofa_send_data(1, Drivers.driver1.speed);
-		vofa_send_data(2, Drivers.driver1.torque);
-		vofa_send_data(3, Drivers.driver2.angle);
-		vofa_send_data(4, Drivers.driver2.speed);
-		vofa_send_data(5, Drivers.driver2.torque);
-		vofa_send_data(6, Drivers.driver3.angle);
-		vofa_send_data(7, Drivers.driver3.speed);
-		vofa_send_data(8, Drivers.driver3.torque);
-		vofa_send_data(9, tension_sensor[0]);
-		vofa_send_data(10, tension_sensor[1]);
-		vofa_send_data(11, tension_sensor[2]);
-		vofa_send_data(12, (float) system_frequency);
-		vofa_send_data(13, (float) program_mode_code);
-		vofa_sendframetail();
-
-		driver_monitoring_flag = 1;
+void HandleScreenReflash() {
+	//系统运行状态提示进度条
+	if (screen_sequence > 90)
+		screen_sequence = 0;
+	if (screen_sequence >= 5)
+		Paint_ClearWindows((screen_sequence - 10) * 3, 133, (screen_sequence - 9) * 3, 134, BLACK);
+	if (screen_sequence <= 80) {
+		if (screen_sequence >= 0 && screen_sequence <= 27) {
+			c_Red = screen_sequence * 9 + 10;
+			c_Green = 0;
+			c_Blue = (27 - screen_sequence) * 9 + 10;
+		}
+		if (screen_sequence > 27 && screen_sequence <= 54) {
+			c_Red = (54 - screen_sequence) * 9 + 10;
+			c_Green = (screen_sequence - 28) * 9 + 10;
+			c_Blue = 0;
+		}
+		if (screen_sequence > 54 && screen_sequence <= 80) {
+			c_Red = 0;
+			c_Green = (80 - screen_sequence) * 9 + 10;
+			c_Blue = (screen_sequence - 55) * 9 + 10;
+		}
+		Paint_ClearWindows(screen_sequence * 3, 133, (screen_sequence + 1) * 3, 134, RGB888ToRGB565(c_Red, c_Green, c_Blue));
 	}
-	if (htim == &htim16) { //屏幕刷新基准时钟100ms
-		HAL_TIM_Base_Start_IT(&htim16);
+	screen_sequence++;
 
-		//系统运行状态提示进度条
-		if (screen_sequence > 90)
-			screen_sequence = 0;
-		if (screen_sequence >= 5)
-			Paint_ClearWindows((screen_sequence - 10) * 3, 133, (screen_sequence - 9) * 3, 134, BLACK);
-		if (screen_sequence <= 80) {
-			if (screen_sequence >= 0 && screen_sequence <= 27) {
-				c_Red = screen_sequence * 9 + 10;
-				c_Green = 0;
-				c_Blue = (27 - screen_sequence) * 9 + 10;
-			}
-			if (screen_sequence > 27 && screen_sequence <= 54) {
-				c_Red = (54 - screen_sequence) * 9 + 10;
-				c_Green = (screen_sequence - 28) * 9 + 10;
-				c_Blue = 0;
-			}
-			if (screen_sequence > 54 && screen_sequence <= 80) {
-				c_Red = 0;
-				c_Green = (80 - screen_sequence) * 9 + 10;
-				c_Blue = (screen_sequence - 55) * 9 + 10;
-			}
-			Paint_ClearWindows(screen_sequence * 3, 133, (screen_sequence + 1) * 3, 134, RGB888ToRGB565(c_Red, c_Green, c_Blue));
-		}
-		screen_sequence++;
+	Home.params[3].num1 = (float) system_frequency / 1000;
+	Home.params[3].num2 = (float) program_mode_code;
 
-		Home.params[3].num1 = (float) system_frequency / 1000;
-		Home.params[3].num2 = (float) program_mode_code;
+	Home.status[0].num1 = tension_sensor[0];
+	Home.status[1].num1 = tension_sensor[1];
+	Home.status[2].num1 = tension_sensor[2];
 
-		if (parameters_reflash_flag) {
-			Parameters_Reflash();
-			parameters_reflash_flag = 0;
-		}
-		if (targets_reflash_flag) {
-			//Targets_Reflash();
-			targets_reflash_flag = 0;
-		}
-		if (status_reflash_flag) {
-			Status_Reflash();
-			status_reflash_flag = 0;
-		}
+	if (parameters_reflash_flag) {
+		Parameters_Reflash();
+		parameters_reflash_flag = 0;
 	}
-	if (htim == &htim7) { //控制循环基准时钟1ms
-		HAL_TIM_Base_Start_IT(&htim7);
+	if (targets_reflash_flag) {
+		//Targets_Reflash();
+		targets_reflash_flag = 0;
+	}
+	if (status_reflash_flag) {
+		Status_Reflash();
+		status_reflash_flag = 0;
+	}
+}
 
-		ADC_Read();
+void HandleControlThread() {
+	static float last_offboard_data[3] = { 0 };
+	ADC_Read();
 //		tension_sensor[0] = (float) ADC_value_2[0] / 4096 * 3.3;	//读取传感器信息
 //		tension_sensor[1] = (float) ADC_value_2[1] / 4096 * 3.3;
 //		tension_sensor[2] = (float) ADC_value_2[2] / 4096 * 3.3;
-		tension_sensor[0] = (float) ADC_value_2_Kalman[0] * 3.3 / 4096;	//读取传感器信息
-		tension_sensor[1] = (float) ADC_value_2_Kalman[1] * 3.3 / 4096;
-		tension_sensor[2] = (float) ADC_value_2_Kalman[2] * 3.3 / 4096;
+	tension_sensor[0] = (float) ADC_value_2_Kalman[0] * 3.3 / 4096;	//读取传感器信息
+	tension_sensor[1] = (float) ADC_value_2_Kalman[1] * 3.3 / 4096;
+	tension_sensor[2] = (float) ADC_value_2_Kalman[2] * 3.3 / 4096;
 
-		//	if (tension_sensor[0] < 0.02 || tension_sensor[1] < 0.02 || tension_sensor[2] < 0.02) {
-		//		HandleWarning(WARNING_TENSION_OUT);
-		//	} else {
-		//		HandleWarning(ERROR_NONE);
-		//	}
+	//	if (tension_sensor[0] < 0.02 || tension_sensor[1] < 0.02 || tension_sensor[2] < 0.02) {
+	//		HandleWarning(WARNING_TENSION_OUT);
+	//	} else {
+	//		HandleWarning(ERROR_NONE);
+	//	}
 
-		switch (program_mode_code) {	//控制循环模式识别
-		case 000:	//空闲
-			break;
-		case 101:	//测试程序1
-			break;
-		case 102:	//测试程序2
-			break;
-		case 103:	//测试程序3
-			break;
-		case 201:	//外部信号控制循环
-			for (int i = 0; i < 3; i++) {
+	switch (program_mode_code) {	//控制循环模式识别
+	case 000:	//空闲
+		break;
+	case 101:	//测试程序1
+		break;
+	case 102:	//测试程序2
+		break;
+	case 103:	//测试程序3
+		break;
+	case 201:	//外部信号控制循环
+		for (int i = 0; i < 3; i++) {
+			if (offboard_data[i] != last_offboard_data[i]) { // 检查数据是否变化
+				last_offboard_data[i] = offboard_data[i]; // 更新记录的值
 				switch (offboard_command[i]) {
 				case 20:
-					if (offboard_data[i] > 1.2) {
+					if (offboard_data[i] > 1.2 || offboard_data[i] < -1.2) {
 						estop(0);
 						program_mode_code = 999;
 						HandleError(ERROR_TORQUE_OUT);
@@ -152,33 +153,53 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 					HandleError(ERROR_SIGNAL_LOST);
 				}
 			}
-			break;
-		case 202:					//自定义控制循环
-			for (int i = 0; i < 3; i++) {
-				set_torque(i + 1, 0.15, 1, 0);					//预紧
-			}
-			break;
-		case 203:					//自定义控制
-			for (int i = 0; i < 3; i++) {
-				if (tension_sensor[i] < 0.1) {
-					set_torque(i + 1, 0.15, 1, 0);					//预紧
-				} else {
-					set_speed(i + 1, PID_Compute(&pid_tension_1, offboard_data[i], tension_sensor[i], 0.001f), 1000, 1);					//预紧
-				}
-			}
-			break;
-		default:
 		}
-
-	}
-	if (htim == &htim6) { //占用率基准时钟1000ms
-		HAL_TIM_Base_Start_IT(&htim6);
-
-		CheckAndHandleErrors();
-
-		system_frequency = system_cycle_counter;
-		system_cycle_counter = 0;
-		system_timeout_flag++;
+		break;
+	case 202:					//自定义控制循环
+//		for (int i = 0; i < 3; i++) {
+//			set_torque(i + 1, 0.15, 1, 0);					//预紧
+//		}
+		break;
+	case 203:					//自定义控制
+//		for (int i = 0; i < 3; i++) {
+//			if (tension_sensor[i] < 0.1) {
+//				set_torque(i + 1, 0.15, 1, 0);					//预紧
+//			} else {
+//				set_speed(i + 1, PID_Compute(&pid_tension_1, offboard_data[i], tension_sensor[i], 0.001f), 1000, 1);					//预紧
+//			}
+//		}
+		break;
+	default:
 	}
 }
 
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	switch ((uint32_t) htim->Instance) {
+	case (uint32_t) TIM17: // 回传基准时钟5ms
+		HAL_TIM_Base_Start_IT(&htim17);
+		HandleDriverMonitoring();
+		break;
+
+	case (uint32_t) TIM16: // 屏幕刷新基准时钟100ms
+		HAL_TIM_Base_Start_IT(&htim16);
+		HandleScreenReflash();
+		break;
+
+	case (uint32_t) TIM7: // 控制循环基准时钟1ms
+		HAL_TIM_Base_Start_IT(&htim7);
+		HandleControlThread();
+		break;
+
+	case (uint32_t) TIM6: // 占用率错误监测基准时钟1000ms
+		HAL_TIM_Base_Start_IT(&htim6);
+		CheckAndHandleErrors();
+		system_frequency = system_cycle_counter;
+		system_cycle_counter = 0;
+		system_timeout_flag++;
+		break;
+
+	default:
+		// 未知定时器实例
+		break;
+	}
+}
